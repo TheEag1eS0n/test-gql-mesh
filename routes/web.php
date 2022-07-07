52c53fc07eae7ad684ac6077339ff5016fe5f70e
TheEag1eS0n <egorfaringate@gmail.com>
2022-07-07 14:22:56 +0500

    asd

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/graphql', function () {
+    return redirect()->route('graphql', [
+        'query' => \GraphQL\Type\Introspection::getIntrospectionQuery(),
+    ]);
+});
